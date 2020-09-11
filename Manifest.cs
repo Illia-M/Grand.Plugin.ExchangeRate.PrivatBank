3655d43a8729f5f63f6d876a2100b833d8163eb2
Illia Maier <illia.maier@pm.bet>
2020-09-11 18:32:06 +0300

    Change to Manifest.cs

---
+++ Manifest.cs
@@ -0,0 +1,11 @@
+﻿using Grand.Core;
+using Grand.Core.Plugins;
+
+[assembly: PluginInfo(
+    FriendlyName = "PrivatBank exchange rate provider",
+    Group = "Exchange rate providers",
+    SystemName = "CurrencyExchange.PrivatBank",
+    SupportedVersion = GrandVersion.SupportedPluginVersion,
+    Author = "Illia Maier",
+    Version = "1.1"
+)]
